05eb632f95cd3cdebf5f7bc9de909f2b9e10768a
0xDED <63008025+dedouwe26@users.noreply.github.com>
2025-07-04 22:54:50 +0200

    quick they are coming

--- src/main/java/oxded/cursors/api/Cursors.java
+++ src/main/java/oxded/cursors/api/Cursors.java
@@ -1,13 +1,38 @@
 package oxded.cursors.api;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.texture.AbstractTexture;
+import net.minecraft.client.texture.TextureManager;
 import net.minecraft.client.util.Window;
+import net.minecraft.util.Identifier;
 import oxded.cursors.CursorsClient;
 
+@Environment(EnvType.CLIENT)
 public final class Cursors {
+    private static final Identifier DEFAULT_CURSOR = Identifier.of("cursors", "textures/cursors/default.png");
+    private static final Identifier POINTER_CURSOR = Identifier.of("cursors", "textures/cursors/pointer.png");
+    private static final Identifier TEXT_CURSOR = Identifier.of("cursors", "textures/cursors/text.png");
+    private static final Identifier GRAB_CURSOR = Identifier.of("cursors", "textures/cursors/grab.png");
+    private static final Identifier GRABBING_CURSOR = Identifier.of("cursors", "textures/cursors/grabbing.png");
+    private static final Identifier BLOCKED_CURSOR = Identifier.of("cursors", "textures/cursors/blocked.png");
 	private static Window window;
-    private static ResourceLocation location;
-    public static void init(Window win) {
+    private static CursorType cursorType;
+    private static AbstractTexture defaultCursor;
+    private static AbstractTexture pointerCursor;
+    private static AbstractTexture textCursor;
+    private static AbstractTexture grabCursor;
+    private static AbstractTexture grabbingCursor;
+    private static AbstractTexture blockedCursor;
+
+    public static void init(Window win, TextureManager textureManager) {
         window = win;
+        defaultCursor = textureManager.getTexture(DEFAULT_CURSOR);
+        pointerCursor = textureManager.getTexture(POINTER_CURSOR);
+        textCursor = textureManager.getTexture(TEXT_CURSOR);
+        grabCursor = textureManager.getTexture(GRAB_CURSOR);
+        grabbingCursor = textureManager.getTexture(GRABBING_CURSOR);
+        blockedCursor = textureManager.getTexture(BLOCKED_CURSOR);
     }
 
     public static Window getWindow() {
@@ -16,6 +41,9 @@ public final class Cursors {
     }
 
     public static void setCursorType(CursorType type) {
-        
+        cursorType = type;
+    }
+    public static CursorType getCursorType() {
+        return cursorType;
     }
 }
