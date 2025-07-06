70e1affea514557291d787ff43a1f0221384c55e
0xDED <63008025+dedouwe26@users.noreply.github.com>
2025-07-06 17:17:38 +0200

    Add all cursors

--- src/client/java/oxded/cursors/CursorsClient.java
+++ src/client/java/oxded/cursors/CursorsClient.java
@@ -4,13 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Window;
 import oxded.cursors.api.Cursors;
 
-@Environment(EnvType.CLIENT)
 public class CursorsClient implements ClientModInitializer {
 	public static final String MOD_ID = "cursors";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
